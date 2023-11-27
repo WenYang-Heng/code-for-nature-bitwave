7a9b4fd51ed915b7a75ee4cca2c1261b2e9f5f16
Heng Wen Yang <wenyang.heng@outlook.com>
2023-11-27 19:18:34 +0800

    added sidebar

--- src/main/java/com/codefornature/HelloApplication.java
+++ src/main/java/com/codefornature/HelloApplication.java
@@ -10,8 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sidebar.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        String css = this.getClass().getClassLoader().getResource("styles/sidebar.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

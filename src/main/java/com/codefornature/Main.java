f9534c2946b7875ebd7179c3fa97290d959044ab
Heng Wen Yang <wenyang.heng@outlook.com>
2023-11-27 19:21:10 +0800

    renamed default main class to Main.java

--- src/main/java/com/codefornature/Main.java
+++ src/main/java/com/codefornature/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sidebar.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sidebar.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
         String css = this.getClass().getClassLoader().getResource("styles/sidebar.css").toExternalForm();
         scene.getStylesheets().add(css);

4c0c7cbcd365bfdfbb27cdb9daee46962287ff2d
Heng Wen Yang <wenyang.heng@outlook.com>
2023-11-30 19:34:28 +0800

    changed default font

--- src/main/java/com/codefornature/Main.java
+++ src/main/java/com/codefornature/Main.java
@@ -1,10 +1,8 @@
 package com.codefornature;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-container.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("root.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

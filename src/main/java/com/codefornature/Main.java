4cc2b0e1c1d726d4fd8ca587c0e95a9e5b90ba1e
Heng Wen Yang <wenyang.heng@outlook.com>
2023-12-05 21:47:34 +0800

    commented root css, cannot edit font size while css is active

--- src/main/java/com/codefornature/Main.java
+++ src/main/java/com/codefornature/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-container.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        scene.getStylesheets().add(getClass().getResource("root.css").toExternalForm());
+//        scene.getStylesheets().add(getClass().getResource("root.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

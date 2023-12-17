6105e57119a3d4a646858e9b6f5808b6eec02047
Heng Wen Yang <wenyang.heng@outlook.com>
2023-12-17 17:09:42 +0800

    news can be cycled with buttons

--- src/main/java/com/codefornature/HomeController.java
+++ src/main/java/com/codefornature/HomeController.java
@@ -3,8 +3,10 @@ package com.codefornature;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -17,6 +19,7 @@ public class HomeController {
     @FXML
     VBox homeContainer;
     private String rootPath;
+    private int visibleColumns = 3;
 
     public void initialize() throws IOException{
         rootPath = System.getProperty("user.dir") + "/src/main/resources/assets/";
@@ -24,7 +27,6 @@ public class HomeController {
     }
 
     public void createNewsUI(){
-        int numberOfColumnsToShow = 3;
         //create the containers
         GridPane newsContainer = new GridPane();
         newsContainer.setStyle("-fx-border-color: green");
@@ -38,25 +40,76 @@ public class HomeController {
         Region region = new Region();
         HBox.setHgrow(region, Priority.ALWAYS);
         Button leftBtn = createCarouselButtons("left-arrow.png");
+        leftBtn.setOnAction(e -> shiftRight(newsContainer));
+
         Button rightBtn = createCarouselButtons("right-arrow.png");
+        rightBtn.setOnAction(e -> shiftLeft(newsContainer));
+
         newsHeader.getChildren().addAll(newsHeaderTitle, region, leftBtn, rightBtn);
         newsHeader.setAlignment(Pos.CENTER);
         newsHeader.setPadding(new Insets(0, 45, 5, 45));
 
-        for(int i = 0; i < 6; i++){
+        for(int i = 0; i < 7; i++){
             VBox newsChild = createNewsVbox(i);
-            GridPane.setColumnIndex(newsChild, i);
-            newsContainer.getChildren().add(newsChild);
+            ColumnConstraints column = new ColumnConstraints();
+            if(i >= 3) {
+                newsChild.setManaged(false);
+                newsChild.setVisible(false);
+            }
+            newsContainer.add(newsChild, i, 0);
         }
 
         // Hide subsequent columns
-        for (int i = numberOfColumnsToShow; i < 6; i++) {
-            ColumnConstraints columnConstraints = new ColumnConstraints();
-            columnConstraints.setPercentWidth(0);
-            newsContainer.getColumnConstraints().add(columnConstraints);
-        }
+//        for (int i = visibleColumns; i < 6; i++) {
+//            ColumnConstraints columnConstraints = new ColumnConstraints();
+//            columnConstraints.setPercentWidth(0);
+//            newsContainer.getColumnConstraints().add(columnConstraints);
+//        }
 
         homeContainer.getChildren().addAll(newsHeader, newsContainer);
+    }
+
+    private void shiftRight(GridPane gridPane){
+        Node lastNode = getNodeFromGridPane(gridPane, gridPane.getChildren().size() - 1, 0);
+
+        for(int i = gridPane.getChildren().size() - 2; i >= 0; i--){
+            Node node = getNodeFromGridPane(gridPane, i, 0);
+            if(node != null){
+                GridPane.setColumnIndex(node, i + 1);
+                if(i == 2 && node.isVisible() == true){
+                    node.setVisible(false);
+                    node.setManaged(false);
+                }
+            }
+        }
+
+        if(lastNode != null){
+            GridPane.setColumnIndex(lastNode, 0);
+            lastNode.setVisible(true);
+            lastNode.setManaged(true);
+        }
+
+    }
+
+    private void shiftLeft(GridPane gridPane){
+
+        Node firstNode = getNodeFromGridPane(gridPane, 0, 0);
+        firstNode.setVisible(false);
+        firstNode.setManaged(false);
+
+        for(int i = 1; i < gridPane.getChildren().size() ; i++){
+            Node node = getNodeFromGridPane(gridPane, i, 0);
+            if(node != null){
+                GridPane.setColumnIndex(node, i - 1);
+                if(i == 3 && node.isVisible() == false){
+                    node.setManaged(true);
+                    node.setVisible(true);
+                }
+            }
+        }
+
+        if(firstNode != null)
+            GridPane.setColumnIndex(firstNode, gridPane.getChildren().size() - 1);
     }
 
     private Button createCarouselButtons(String iconPath){
@@ -96,9 +149,6 @@ public class HomeController {
         newsTitle.getStyleClass().add("newsTitle");
         newsTitle.setPadding(new Insets(10, 5, 0, 5));
 
-        if(index >= 3){
-            newsChild.setManaged(false); //ignore size and layout
-        }
         //adding nodes to respective containers
         newsChild.getChildren().addAll(dateTimeContainer, newsImage, newsTitle);
         return newsChild;
@@ -117,5 +167,14 @@ public class HomeController {
         hbox.setAlignment(Pos.CENTER);
         hbox.setSpacing(4);
         return hbox;
+    }
+
+    private Node getNodeFromGridPane(GridPane gridPane, int col, int row){
+        for(Node node : gridPane.getChildren()){
+            if(GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row){
+                return node;
+            }
+        }
+        return null;
     }
 }

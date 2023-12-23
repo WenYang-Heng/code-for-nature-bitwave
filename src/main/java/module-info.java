0fc55f7d576f3f4bb099bb8da6bfc65fafb7d397
Heng Wen Yang <wenyang.heng@outlook.com>
2023-12-23 20:02:52 +0800

    added Joda-Time library as dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.codefornature {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.joda.time;
 
     opens com.codefornature to javafx.fxml;
     exports com.codefornature;
